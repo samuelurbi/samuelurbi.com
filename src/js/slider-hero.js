1fba739779db04620a72dd59e848338fc94ef8f8
git@github.com <samuelurbi93@gmail.com>
2022-09-15 08:40:42 -0400

    Add Slider

---
+++ src/js/slider-hero.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 4,
+    spaceBetween: 0,
+
+    autoplay: {
+      delay: 2500,
+    },
+  });
