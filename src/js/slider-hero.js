602e3223530a7d2bf67bb58a8f96e37b9495a90d
git@github.com <samuelurbi93@gmail.com>
2022-09-18 20:44:28 -0400

    Add Font Test

--- src/js/slider-hero.js
+++ src/js/slider-hero.js
@@ -1,12 +1,24 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+  // Default parameters
+  direction: 'horizontal',
+  loop: true,
 
-    slidesPerView: 4,
-    spaceBetween: 0,
+  slidesPerView: 2,
+  spaceBetween: 0,
+  
+  autoplay: {
+    delay: 2500,
+  },
 
-    autoplay: {
-      delay: 2500,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 768px
+    769: {
+      slidesPerView: 3
     },
-  });
+    // when window width is >= 1024px
+    1025: {
+      slidesPerView: 4
+    }
+  }
+});
